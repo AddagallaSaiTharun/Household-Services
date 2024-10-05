489e3e761c4a3df7ed0c524388a70a73903f3fd9
slvarun <varun.merugu@yahoo.in>
2024-10-05 13:46:10 +0530

    Implemented O-Auth

--- application/controller/controllers.py
+++ application/controller/controllers.py
@@ -1,5 +1,35 @@
 from flask import current_app as app
+from flask import Flask, render_template, url_for, redirect
+from application.data.database import db
+from authlib.integrations.flask_client import OAuth
+from application.config import oAuth_cred
 
-@app.route('/', methods=['GET'])
-def home():
-    return("Hello World")
+
+oauth  = OAuth(app)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/google/')
+def google():
+    GOOGLE_CLIENT_ID = oAuth_cred.GOOGLE_CLIENT_ID
+    GOOGLE_CLIENT_SECRET = oAuth_cred.GOOGLE_CLIENT_SECRET
+    CONF_URL = oAuth_cred.CONF_URL
+    oauth.register(
+        name='google',
+        client_id=GOOGLE_CLIENT_ID,
+        client_secret=GOOGLE_CLIENT_SECRET,
+        server_metadata_url=CONF_URL,
+        client_kwargs={
+            'scope': 'openid email profile'
+        }
+    )
+    redirect_uri = url_for('google_auth', _external=True)
+    return oauth.google.authorize_redirect(redirect_uri)
+ 
+@app.route('/google/auth/')
+def google_auth():
+    token = oauth.google.authorize_access_token()
+    user = token.get('userinfo')
+    return redirect('/')
+ 
